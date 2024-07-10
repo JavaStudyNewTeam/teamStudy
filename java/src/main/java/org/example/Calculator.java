4eac08b12b870e38162ea8c32fa7ceb5474da729
gustmd9146 <gustmd9146@gmail.com>
2024-07-11 00:06:29 +0900

    1111

---
+++ java/src/main/java/org/example/Calculator.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculator {
+
+    public void divideTwoNumber(int a, int b) {
+        System.out.println("a / b = " + a / b);
+
+
+    }
+    public void minusTwoNumber(int a , int b) {
+        System.out.println("a - b = " + (a - b) );
+    }
+
+
+}
